578d532e93f03ec2e31e5fcd9fd29ea9ded1a1e2
rofergon <sebas.2023@gmail.com>
2026-02-17 11:00:02 -0500

    refactor(interfaces): update ICompliance for ERC-3643 compliance
    Update the ICompliance interface to include standard binding and agent
    management functions (bindToken, unbindToken, isTokenBound).

--- starter-templates/tokenized-asset-servicing/contracts/equity-protocol/ICompliance.sol
+++ starter-templates/tokenized-asset-servicing/contracts/equity-protocol/ICompliance.sol
@@ -31,4 +31,40 @@ interface ICompliance {
      * @param _amount The amount of tokens burned.
      */
     function destroyed(address _from, uint256 _amount) external;
+
+    /**
+     * @dev Binds a token to the compliance contract.
+     * @param _token The address of the token to bind.
+     */
+    function bindToken(address _token) external;
+
+    /**
+     * @dev Unbinds a token from the compliance contract.
+     * @param _token The address of the token to unbind.
+     */
+    function unbindToken(address _token) external;
+
+    /**
+     * @dev Checks if a token is bound to the compliance contract.
+     * @param _token The address of the token to check.
+     */
+    function isTokenBound(address _token) external view returns (bool);
+
+    /**
+     * @dev Adds a token agent to the compliance contract.
+     * @param _agentAddress The address of the agent to add.
+     */
+    function addTokenAgent(address _agentAddress) external;
+
+    /**
+     * @dev Removes a token agent from the compliance contract.
+     * @param _agentAddress The address of the agent to remove.
+     */
+    function removeTokenAgent(address _agentAddress) external;
+
+    /**
+     * @dev Checks if an address is a token agent.
+     * @param _agentAddress The address to check.
+     */
+    function isTokenAgent(address _agentAddress) external view returns (bool);
 }
