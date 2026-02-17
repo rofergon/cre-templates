f828b497ffc89fe328ac19d2212a56903f9b2aab
rofergon <sebas.2023@gmail.com>
2026-02-17 17:50:28 -0500

    feat: Add `Compliance` contract to manage token binding, token agents, and basic transfer compliance checks for tokenized assets.

---
+++ starter-templates/tokenized-asset-servicing/contracts/equity-protocol/Compliance.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ICompliance.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Compliance is ICompliance, Ownable {
+    // Mapping to store bound tokens
+    mapping(address => bool) private _boundTokens;
+    // Mapping to store token agents
+    mapping(address => bool) private _tokenAgents;
+
+    modifier onlyTokenAgent() {
+        require(_tokenAgents[msg.sender], "Caller is not a token agent");
+        _;
+    }
+
+    modifier onlyBoundToken() {
+        require(_boundTokens[msg.sender], "Caller is not a bound token");
+        _;
+    }
+
+    constructor() {}
+
+    /**
+     * @dev Checks if a transfer can be executed.
+     * @param _from The address of the sender.
+     * @param _to The address of the receiver.
+     * @param _amount The amount of tokens to transfer.
+     */
+    function canTransfer(address _from, address _to, uint256 _amount) external view override returns (bool) {
+        // Simple compliance logic: allow all transfers by default.
+        // In a real-world scenario, this would include checks for KYC, country restrictions, etc.
+        return true;
+    }
+
+    /**
+     * @dev Called by the token contract before a transfer to update compliance state.
+     * @param _from The address of the sender.
+     * @param _to The address of the receiver.
+     * @param _amount The amount of tokens to transfer.
+     */
+    function transferred(address _from, address _to, uint256 _amount) external override onlyBoundToken {
+        // Logic to update compliance state after a transfer
+    }
+
+    /**
+     * @dev Called by the token contract on minting.
+     * @param _to The address of the receiver.
+     * @param _amount The amount of tokens minted.
+     */
+    function created(address _to, uint256 _amount) external override onlyBoundToken {
+        // Logic to update compliance state after minting
+    }
+
+    /**
+     * @dev Called by the token contract on burning.
+     * @param _from The address of the burner.
+     * @param _amount The amount of tokens burned.
+     */
+    function destroyed(address _from, uint256 _amount) external override onlyBoundToken {
+        // Logic to update compliance state after burning
+    }
+
+    /**
+     * @dev Binds a token to the compliance contract.
+     * @param _token The address of the token to bind.
+     */
+    function bindToken(address _token) external override onlyOwner {
+        _boundTokens[_token] = true;
+    }
+
+    /**
+     * @dev Unbinds a token from the compliance contract.
+     * @param _token The address of the token to unbind.
+     */
+    function unbindToken(address _token) external override onlyOwner {
+        _boundTokens[_token] = false;
+    }
+
+    /**
+     * @dev Checks if a token is bound to the compliance contract.
+     * @param _token The address of the token to check.
+     */
+    function isTokenBound(address _token) external view override returns (bool) {
+        return _boundTokens[_token];
+    }
+
+    /**
+     * @dev Adds a token agent to the compliance contract.
+     * @param _agentAddress The address of the agent to add.
+     */
+    function addTokenAgent(address _agentAddress) external override onlyOwner {
+        _tokenAgents[_agentAddress] = true;
+    }
+
+    /**
+     * @dev Removes a token agent from the compliance contract.
+     * @param _agentAddress The address of the agent to remove.
+     */
+    function removeTokenAgent(address _agentAddress) external override onlyOwner {
+        _tokenAgents[_agentAddress] = false;
+    }
+
+    /**
+     * @dev Checks if an address is a token agent.
+     * @param _agentAddress The address to check.
+     */
+    function isTokenAgent(address _agentAddress) external view override returns (bool) {
+        return _tokenAgents[_agentAddress];
+    }
+}
