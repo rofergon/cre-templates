5c15e703941c5227cdbdfdd6b071b235090c3d70
rofergon <sebas.2023@gmail.com>
2026-02-17 17:02:01 -0500

    feat: Add initial Hardhat configuration, including baseSepolia network setup and source paths.

--- starter-templates/tokenized-asset-servicing/contracts/hardhat.config.cjs
+++ starter-templates/tokenized-asset-servicing/contracts/hardhat.config.cjs
@@ -1,14 +1,14 @@
 require("@nomicfoundation/hardhat-ethers");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
-// require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: "../.env" });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
   networks: {
     baseSepolia: {
-      url: "https://sepolia.basescan.org",
+      url: "https://base-sepolia.gateway.tenderly.co/3qeYD3iE02OOzPOCANms01",
       accounts: process.env.CRE_ETH_PRIVATE_KEY ? [process.env.CRE_ETH_PRIVATE_KEY] : [],
     },
   },
