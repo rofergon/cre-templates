2032b6a532fef5fa4a368134ef011a2769611f5a
rofergon <sebas.2023@gmail.com>
2026-02-22 12:42:50 -0500

    chore: Delete file.

--- starter-templates/tokenized-asset-servicing/contracts/scripts/debug-config.cjs
+++
@@ -1,7 +0,0 @@
-try {
-    require('dotenv').config({ path: '../.env' });
-    const config = require('../hardhat.config.cjs');
-    console.log("Config loaded successfully:", config);
-} catch (error) {
-    console.error("Error loading config:", error);
-}
