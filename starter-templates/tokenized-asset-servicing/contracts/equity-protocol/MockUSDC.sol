e2e2e6b04ee53f821e78e7c88e4c779c4184ca9e
rofergon <sebas.2023@gmail.com>
2026-02-24 21:50:34 -0500

    feat: add MockUSDC contract for market settlement simulations

---
+++ starter-templates/tokenized-asset-servicing/contracts/equity-protocol/MockUSDC.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title MockUSDC
+/// @notice 6-decimals mintable ERC20 for market settlement simulations.
+contract MockUSDC is ERC20, Ownable {
+    constructor() ERC20("Mock USD Coin", "mUSDC") {}
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
